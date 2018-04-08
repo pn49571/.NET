da205f49aac1bc570d9d2e25474dbb7e165bb2b0
Saiadithya (The Sun God) <pn49571@umbc.edu>
2018-04-07 20:10:13 -0700

    Reverse string

---
+++ Basic_Problems/Reverse_string/Reverse_string/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reverse_string
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(StringHelper.ReverseString("framework"));
+            Console.WriteLine(StringHelper.ReverseString("samuel"));
+            Console.WriteLine(StringHelper.ReverseString("example string"));
+        }
+        static class StringHelper
+        {
+            /// <summary>
+            /// Receives string and returns the string with its letters reversed.
+            /// </summary>
+            public static string ReverseString(string s)
+            {
+                char[] arr = s.ToCharArray();
+                Array.Reverse(arr);
+                return new string(arr); //array to string convert
+            }
+        }
+    }
+}
